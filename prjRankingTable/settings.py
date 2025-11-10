179732ece296dda2bfc3afcf2601ed504f7dc099
Geencuve <165859258+Geencuve@users.noreply.github.com>
2025-11-10 02:56:30 -0300

    Update settings.py

--- prjRankingTable/settings.py
+++ prjRankingTable/settings.py
@@ -27,7 +27,9 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
-STATIC_ROOT = [os.path.join(BASE_DIR, 'staticfiles')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
