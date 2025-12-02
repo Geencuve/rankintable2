be311b8cd0458d83cb0d2bebd6f9b00d11f56be2
Geencuve <165859258+Geencuve@users.noreply.github.com>
2025-12-01 23:49:37 -0300

    Update settings.py

--- prjRankingTable/settings.py
+++ prjRankingTable/settings.py
@@ -99,11 +99,11 @@ WSGI_APPLICATION = 'prjRankingTable.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': config('DB_NAME', default='rankingtabledb'),
-        'USER': config('DB_USER', default='root'),
-        'PASSWORD': config('DB_PASSWORD', default='admin'),
-        'HOST': config('DB_HOST', default='web-production-0257.up.railway.app'),
-        'PORT': config('DB_PORT', default='3306'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT', '3306'),
     }
 }
 
